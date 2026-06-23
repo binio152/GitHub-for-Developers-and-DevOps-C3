143fe058b0da05b93142c288909d5f56e25b712f
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-24 02:42:45 +0700

    feat(17-2): update workflow using input output with js

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,18 @@
 import * as core from "@actions/core";
+import fs from "fs";
 
 async function run() {
-  core.info("I'm a custom JS action");
+  const nameToGreet = process.env["INPUT_WHO_TO_GREET"];
+  const message = `Hello ${nameToGreet}`;
+  console.log(message);
+
+  const time = new Date().toTimeString();
+  console.log(`Greeting at: ${time}`);
+
+  fs.appendFile(
+    process.env.GITHUB_OUTPUT,
+    `logging=Say${message} at ${time}\n`,
+  );
 }
 
 run();
