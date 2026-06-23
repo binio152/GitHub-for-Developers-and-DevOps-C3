d521ed5302673b3767598d7762891af5521e147e
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-24 02:58:33 +0700

    feat(17-2): update workflow with actions/core

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -2,17 +2,18 @@ import * as core from "@actions/core";
 import fs from "fs";
 
 async function run() {
-  const nameToGreet = process.env["INPUT_WHO_TO_GREET"];
-  const message = `Hello ${nameToGreet}`;
-  console.log(message);
+  try {
+    const nameToGreet = core.getInput("who-to-greet");
+    const message = `Hello ${nameToGreet}`;
+    core.info(message);
 
-  const time = new Date().toTimeString();
-  console.log(`Greeting at: ${time}`);
+    const time = new Date().toTimeString();
+    core.info(`Greeting at: ${time}`);
 
-  fs.appendFile(
-    process.env.GITHUB_OUTPUT,
-    `logging=Say${message} at ${time}\n`,
-  );
+    core.setOutput("logging", `Say${message} at ${time}`);
+  } catch (err) {
+    core.setFailed(err.message);
+  }
 }
 
 run();
