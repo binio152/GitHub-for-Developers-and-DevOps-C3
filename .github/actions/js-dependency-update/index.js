c8d0eeaf0f20327c32d60ffd17c7d40c54bcf9f9
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-25 00:18:10 +0700

    Update index.js

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -2,7 +2,14 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 async function run() {
   try {
-    console.log(github.context);
+    console.log(`SHA: ${github.context.sha}`);
+    console.log(`Branch: ${github.context.ref}`);
+    console.log(`Trigger event: ${github.context.eventName}`);
+    console.log(`Trigger user: ${github.context.actor}`);
+    console.log(`Workflow name: ${github.context.workflow}`);
+    console.log(`Repo owner: ${github.context.repo.owner}`);
+    console.log(`Job name: ${github.context.job}`);
+    console.log(`Payload: ${github.context.payload}`);
   } catch (err) {
     core.setFailed(err.message);
   }
