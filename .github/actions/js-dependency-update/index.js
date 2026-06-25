b9781cd8f3780b8c2b25a879a6650656cc7ecb96
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-25 15:33:22 +0700

    update workflow

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -4,36 +4,42 @@ import * as github from "@actions/github";
 async function run() {
   try {
     const context = github.context;
-    const action = context.eventName;
-    const payload = context.payload;
+    // const action = context.eventName;
+    // const payload = context.payload;
 
-    switch (action) {
-      case action === "push":
-        core.info(
-          JSON.stringify({
-            Pusher: `${payload.pusher.name} <${payload.pusher.email}>`,
-            "Pushed branch": `${payload.ref}`,
-          }),
-        );
-        for (const commit of payload.commits ?? []) {
-          core.info(`Commit ${commit.id}: ${commit.message}`);
-        }
-        break;
+    core.info(JSON.stringify(context));
 
-      case "pull_request":
-        core.info(JSON.stringify(payload));
-        break;
+    // switch (action) {
+    //   case action === "push":
+    //     core.info(
+    //       JSON.stringify({
+    //         Pusher: `${payload.pusher.name} <${payload.pusher.email}>`,
+    //         "Pushed branch": `${payload.ref}`,
+    //       }),
+    //     );
+    //     for (const commit of payload.commits ?? []) {
+    //       core.info(`Commit ${commit.id}: ${commit.message}`);
+    //     }
+    //     break;
 
-      case "issues":
-        core.info(
-          JSON.stringify({
-            "Issue state": `${payload.state}`,
-            "Issue title": `${payload.title}`,
-            "Issue body": `${payload.body}`,
-          }),
-        );
-        break;
-    }
+    //   case "pull_request":
+    //     core.info(JSON.stringify(payload));
+    //     break;
+
+    //   case "issue":
+    //     core.info(
+    //       JSON.stringify({
+    //         "Issue state": `${payload.issue.state}`,
+    //         "Issue title": `${payload.issue.title}`,
+    //         "Issue body": `${payload.issue.body}`,
+    //       }),
+    //     );
+
+    //     for (const label of payload.issue.labels ?? []) {
+    //       core.info(label);
+    //     }
+    //     break;
+    // }
   } catch (err) {
     core.setFailed(err.message);
   }
