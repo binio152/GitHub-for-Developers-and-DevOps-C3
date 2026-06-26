8add6796107b6a34e0b21f92e72c295a71540ceb
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-26 18:07:17 +0700

    completed workflow for dependency update

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -7,61 +7,99 @@ const validateBranchName = (branchName) =>
 const validateWorkingDirectory = (directory) =>
   /^[a-zA-z0-9_\-\.\/]+$/.test(directory);
 
+async function setUpGit() {
+  await exec.exec(`git config --global user.name "gh-automation"`);
+  await exec.exec(`git config --global user.email "gh-automation@gmail.com"`);
+}
+
+function getInput() {
+  const token = core.getInput("github-token", { required: true });
+  const baseBranch = core.getInput("base-branch", { required: true });
+  const headBranch = core.getInput("head-branch", { required: true });
+  const cwd = core.getInput("working-directory", {
+    required: true,
+  });
+  const debug = core.getBooleanInput("debug");
+
+  return { token, baseBranch, headBranch, cwd, debug };
+}
+
+function createOctokit(token) {
+  core.setSecret(token);
+  const octokit = github.getOctokit(token);
+  return octokit;
+}
+
+function validateBranch(base, head) {
+  if (!validateBranchName(base)) {
+    core.setFailed("Invalid base branch");
+    return;
+  }
+  if (!validateBranchName(head)) {
+    core.setFailed("Invalid head branch");
+    return;
+  }
+}
+
+function validateWorkingDirectory(workingDirectory) {
+  if (!validateWorkingDirectory(workingDirectory)) {
+    core.setFailed("Invalid working directory");
+    return;
+  }
+}
+
+const setupLogger = ({ debug, prefix } = { debug: false, prefix: "" }) => ({
+  debug: (message) => {
+    if (debug) {
+      core.info(`DEBUG ${prefix}${prefix ? ":" : ""}${message}`);
+    }
+  },
+  error: (message) => {
+    core.error(`${prefix}${prefix ? ":" : ""}${message}`);
+  },
+});
+
 async function run() {
   try {
-    const token = core.getInput("github-token", { required: true });
-    const baseBranch = core.getInput("base-branch", { required: true });
-    const targetBranch = core.getInput("target-branch", { required: true });
-    const workingDirectory = core.getInput("working-directory", {
-      required: true,
-    });
-    const debug = core.getBooleanInput("debug");
-
-    const execOpts = { cwd: workingDirectory };
-
-    core.setSecret(token);
-    const octokit = github.getOctokit(token);
+    const { token, baseBranch, headBranch, cwd, debug } = getInput();
     const { owner, repo } = github.context.repo;
+    const execOpts = { cwd };
+    const logger = setupLogger({ debug, prefix: "[js-dependency-update]" });
 
-    if (!validateBranchName(baseBranch)) {
-      core.setFailed("Invalid base branch");
-      return;
-    }
-    if (!validateBranchName(targetBranch)) {
-      core.setFailed("Invalid target branch");
-      return;
-    }
-    if (!validateWorkingDirectory(workingDirectory)) {
-      core.setFailed("Invalid working directory");
-      return;
-    }
+    const octokit = createOctokit();
 
-    core.info(`[js-dependencies-update]: base branch is ${baseBranch}`);
-    core.info(`[js-dependencies-update]: target branch is ${targetBranch}`);
-    core.info(
-      `[js-dependencies-update]: working directory is ${workingDirectory}`,
+    logger.debug("Validating base branch, head branch, working directory ...");
+    validateBranch(baseBranch, headBranch);
+    validateWorkingDirectory(cwd);
+    logger.debug(`Base branch is ${baseBranch}`);
+    logger.debug(`Target branch is ${targetBranch}`);
+    logger.debug(`Working directory is ${workingDirectory}`);
+    logger.debug(
+      "Validated base branch, head branch, working directory successfully",
     );
 
+    logger.debug("Running update dependencies ...");
     await exec.exec("npm update", [], { ...execOpts });
 
+    logger.debug("Checking status package-*.json ...");
     const gitStatus = await exec.getExecOutput(
-      "git status -s package*.json",
+      "git status -s package-*.json",
       [],
       { ...execOpts },
     );
 
     if (gitStatus.stdout.length > 0) {
-      core.info(`[js-dependencies-update]: There are updates available!`);
+      logger.debug(`There are updates available!`);
 
       // setup git config
-      await exec.exec(`git config --global user.name "gh-automation"`);
-      await exec.exec(
-        `git config --global user.email "gh-automation@gmail.com"`,
-      );
+      logger.debug(`Setting up git config ...`);
+      setUpGit();
 
       // create new target branch
+      logger.debug(`Creating new ${headBranch} branch ...`);
       await exec.exec(`git switch -c ${targetBranch}`, [], { ...execOpts });
 
+      logger.debug(`Adding package-lock.json and committing ...`);
       // add new package-lock.json and commit
       await exec.exec(`git add package-lock.json`, [], { ...execOpts });
       await exec.exec(`git commit -m "chore: update dependencies"`, [], {
@@ -69,13 +107,16 @@ async function run() {
       });
 
       // push to the target branch
+      logger.debug(
+        `Pushing from ${headBranch} branch to ${baseBranch} branch ...`,
+      );
       await exec.exec(`git push -u ${targetBranch} --force`, [], {
         ...execOpts,
       });
 
       try {
-        core.info("Creating Pull Request ...");
-        await octokit.rest.pulls.create({
+        logger.debug("Creating Pull Request ...");
+        const pr = await octokit.rest.pulls.create({
           owner,
           repo,
           title: `Update NPM dependencies`,
@@ -83,14 +124,13 @@ async function run() {
           head: targetBranch,
           base: baseBranch,
         });
+        logger.debug(`Created PR (${pr.title}: ${pr.body}) `);
       } catch (error) {
-        core.warning(
-          `[js-dependency-update]: Something went srong while creating the PR`,
-        );
+        logger.error(`Something went srong while creating the PR`);
         core.setFailed(error.message);
       }
     } else {
-      core.info(`[js-dependencies-update]: There is no updates at this time`);
+      logger.debug(`There is no updates at this time`);
     }
   } catch (error) {
     core.setFailed(`Error: ${error.message}`);
