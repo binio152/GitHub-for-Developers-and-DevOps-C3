0719ff3b2c8300cd963f50bf25de31b6120d16c1
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-26 16:15:15 +0700

    update workflow

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,38 +1,60 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import * as exec from "@actions/exec";
+
+const validateBranchName = (branchName) =>
+  /^[a-zA-z0-9_\-\.\/]+$/.test(branchName);
+const validateWorkingDirectory = (directory) =>
+  /^[a-zA-z0-9_\-\.\/]+$/.test(directory);
 
 async function run() {
   try {
-    const token = core.getInput("github-token", {required: true});
-    const octokit = github.getOctokit(token);
+    const token = core.getInput("github-token", { required: true });
+    const baseBranch = core.getInput("base-branch");
+    const targetBranch = core.getInput("target-branch");
+    const workingDirectory = core.getInput("working-directory");
+    const debug = core.getBooleanInput("debug");
 
-    const { owner, repo } = github.context.repo;
+    core.setSecret(token);
 
-    if (github.context.eventName === "issues") {
-      await octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number: github.context.payload.issue.number,
-        body: "Thanks for opening this issue!",
-      });
+    if (!validateBranchName(baseBranch)) {
+      core.setFailed("Invalid base branch");
+      return;
+    }
+    if (!validateBranchName(targetBranch)) {
+      core.setFailed("Invalid target branch");
+      return;
+    }
+    if (!validateWorkingDirectory(workingDirectory)) {
+      core.setFailed("Invalid working directory");
+      return;
     }
 
-    if (github.context.eventName === "push") {
-      const pusher = github.context.payload.pusher.name;
-      const ref = github.context.payload.ref;
+    core.info(`[js-dependencies-update]: base branch is ${baseBranch}`);
+    core.info(`[js-dependencies-update]: target branch is ${targetBranch}`);
+    core.info(
+      `[js-dependencies-update]: working directory is ${workingDirectory}`,
+    );
 
-      await octokit.rest.repos.createCommitStatus({
-        owner,
-        repo,
-        sha: github.context.sha,
-        state: "success",
-        target_url: `https://github.com{owner}/${repo}/actions`,
-        description: `Push from ${pusher}`,
-        context: "Security/Pusher-Verification",
-      });
+    await exec.exec("npm update", [], {
+      cwd: workingDirectory,
+    });
+
+    const gitStatus = await exec.getExecOutput(
+      "git status -s package*.json",
+      [],
+      {
+        cwd: workingDirectory,
+      },
+    );
+
+    if (gitStatus.stdout.length > 0) {
+      core.info(`[js-dependencies-update]: There are updates available!`);
+    } else {
+      core.indo(`[js-dependencies-update]: There is no updates at this time`);
     }
 
-    core.info("Actions run successfully");
+    
   } catch (error) {
     core.setFailed(`Error: ${error.message}`);
   }
